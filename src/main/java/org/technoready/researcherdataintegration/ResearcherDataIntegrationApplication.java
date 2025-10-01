4e44b1cc216c7c065466dcb84d7a25c0b6e47e68
ckacy01 <ckacy01@protonmail.com>
2025-10-01 10:10:37 -0600

    initial commit: creating the structure of a spring boot project with maven dependencies

---
+++ src/main/java/org/technoready/researcherdataintegration/ResearcherDataIntegrationApplication.java
@@ -0,0 +1,13 @@
+package org.technoready.researcherdataintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearcherDataIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResearcherDataIntegrationApplication.class, args);
+    }
+
+}
