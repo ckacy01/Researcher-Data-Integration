3aeda8fa389b1f765710c5edc75ba1ed182aa956
ckacy01 <ckacy01@protonmail.com>
2025-10-02 10:08:57 -0600

    feat: Adding the model Interest to save the interests of the author model

---
+++ src/main/java/org/technoready/researcherdataintegration/models/Interest.java
@@ -0,0 +1,11 @@
+package org.technoready.researcherdataintegration.models;
+
+
+import lombok.Data;
+
+@Data
+public class Interest {
+    private String title;
+    private String link;
+    private String serpapi_link;
+}
