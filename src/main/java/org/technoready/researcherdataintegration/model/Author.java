a52b08ea4e5754e1398a95c6f28e86964a237bcc
ckacy01 <ckacy01@protonmail.com>
2025-10-02 21:47:57 -0600

    feat: Adding JsonIgnore

--- src/main/java/org/technoready/researcherdataintegration/model/Author.java
+++ src/main/java/org/technoready/researcherdataintegration/model/Author.java
@@ -2,6 +2,7 @@ package org.technoready.researcherdataintegration.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,6 +13,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Author {
     @JsonProperty("name")
     private String name;
