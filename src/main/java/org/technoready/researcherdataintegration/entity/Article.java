0e73bd98fa6096daa25ae22633bfb87f463e3c21
ckacy01 <ckacy01@protonmail.com>
2025-10-07 23:18:52 -0600

    fix: Adding @Builder

--- src/main/java/org/technoready/researcherdataintegration/entity/Article.java
+++ src/main/java/org/technoready/researcherdataintegration/entity/Article.java
@@ -3,6 +3,7 @@ package org.technoready.researcherdataintegration.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -24,6 +25,7 @@ import java.util.Date;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Article {
 
     @Id
