acfcfbd92b1919bfca69ff2457ec8ab43018bc9b
ckacy01 <ckacy01@protonmail.com>
2025-10-07 23:16:31 -0600

    fix: Correcting the method

--- src/main/java/org/technoready/researcherdataintegration/repository/ArticleRepository.java
+++ src/main/java/org/technoready/researcherdataintegration/repository/ArticleRepository.java
@@ -1,7 +1,6 @@
 package org.technoready.researcherdataintegration.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.technoready.researcherdataintegration.entity.Article;
 
 import java.util.List;
@@ -9,5 +8,5 @@ import java.util.List;
 public interface ArticleRepository  extends JpaRepository<Article, String> {
 
     List<Article> findByResearcherName(String ResearcherName);
-    boolean existsByResearcherName(String id, String researcherName);
+    boolean existsByIdAndResearcherName(String id, String researcherName);
 }
