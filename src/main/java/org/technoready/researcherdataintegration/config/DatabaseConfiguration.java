1dff50afe3b097f35cc7e85344e4f342e1741055
ckacy01 <ckacy01@protonmail.com>
2025-10-07 21:28:06 -0600

    feat: adding the databaseConfiguration class, to get the properties from config.properties

---
+++ src/main/java/org/technoready/researcherdataintegration/config/DatabaseConfiguration.java
@@ -0,0 +1,16 @@
+package org.technoready.researcherdataintegration.config;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "spring.datasource")
+public class DatabaseConfiguration {
+    private String url;
+    private String username;
+    private String password;
+    private String driver;
+}
