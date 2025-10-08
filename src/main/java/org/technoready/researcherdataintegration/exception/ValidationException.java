3ea8ff85b5edb218b4999c22978a28e7da0efeba
ckacy01 <ckacy01@protonmail.com>
2025-10-07 21:45:22 -0600

    feat: Adding the validationExeption

---
+++ src/main/java/org/technoready/researcherdataintegration/exception/ValidationException.java
@@ -0,0 +1,8 @@
+package org.technoready.researcherdataintegration.exception;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+    public ValidationException(String message, Throwable cause) {}
+}
